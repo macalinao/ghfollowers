cf276f0c0ee757287bc86a5b5fb78def38a73338
Ian Macalinao <me@ian.pw>
2014-12-22 14:07:28 -0600

    Add auth

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var _ = require('lodash');
 var express = require('express');
+var session = require('express-session');
+
+var GitHubApi = require('github');
 var githubOAuth = require('github-oauth')({
   githubClient: process.env.GITHUB_CLIENT_ID,
   githubSecret: process.env.GITHUB_SECRET,
@@ -11,19 +14,57 @@ var githubOAuth = require('github-oauth')({
 
 var app = express();
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Get GitHub user
+app.use(function(req, res, next) {
+  if (req.session.token) {
+    var github = req.github = newGithub(req.session.token);
+    if (!req.session.user) {
+      return github.user.get({}, function(err, user) {
+        req.session.user = user;
+        next();
+      });
+    }
+    return next();
+  }
+  next();
+});
+
 app.use(express.static('dist/'));
 
-app.get('/login', githubOAuth.login);
-app.get('/oauth_callback', githubOAuth.callback);
+app.get('/user', function(req, res) {
+  if (!req.session.user) {
+    return res.status(401).json({
+      error: 'Not logged in'
+    });
+  }
+  return res.json(req.session.user);
+});
 
-githubOAuth.on('error', function(err) {
-  console.error('there was a login error', err)
-})
+githubOAuth.addRoutes(app, function(err, token, res, ignore, req) {
+  if (token.error) {
+    return res.send('There was an error logging in: ' + token.error_description);
+  }
 
-githubOAuth.on('token', function(token, serverResponse) {
-  console.log('here is your shiny new github oauth token', token)
-  serverResponse.end(JSON.stringify(token))
-})
+  req.session.token = token.access_token;
+  res.redirect('/');
+});
+
+function newGithub(token) {
+  var api = new GitHubApi({
+    version: '3.0.0'
+  });
+  api.authenticate({
+    type: 'oauth',
+    token: token
+  });
+  return api;
+}
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
