586f30d1c43e23704b2f0d56326423fd1702f901
Ian Macalinao <me@ian.pw>
2014-12-22 23:20:18 -0600

    Move more routes and general code cleanup

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-var _ = require('lodash');
 var express = require('express');
-var db = require('./lib/db');
-var users = db.get('users');
-var session = require('express-session');
-
-var githubOAuth = require('./lib/github_oauth');
 var app = express();
-var newGithub = require('./lib/new_github');
-var addFollowers = require('./lib/follow');
 
-app.use(session({
+// Enable sessions
+app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
@@ -21,19 +14,13 @@ app.use(require('prerender-node'));
 // Middleware to add GH user to request object
 app.use(require('./lib/github_user_middleware'));
 
+// SPA
 app.use(express.static('dist/'));
 
+// Routes
 require('./lib/routes')(app);
 
-githubOAuth.addRoutes(app, function(err, token, res, ignore, req) {
-  if (token.error) {
-    return res.send('There was an error logging in: ' + token.error_description);
-  }
-
-  req.session.token = token.access_token;
-  res.redirect('/');
-});
-
+// Bind to port
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Listening on port ' + port);
