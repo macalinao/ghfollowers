075f67bc1e2167d781f7ded7c0b0a5952e6af5a5
Ian Macalinao <me@ian.pw>
2014-12-22 23:16:05 -0600

    Move GH user middleware to its own file

--- app.js
+++ app.js
@@ -15,39 +15,11 @@ app.use(session({
   saveUninitialized: true
 }));
 
+// Prerender for SEO
 app.use(require('prerender-node'));
 
-// Get GitHub user
-app.use(function(req, res, next) {
-  if (req.session.token) {
-    var github = req.github = newGithub(req.session.token);
-    if (!req.session.user) {
-      return github.user.get({}, function(err, user) {
-        req.session.user = user;
-
-        users.findOne({
-          login: user.login
-        }, function(err, doc) {
-          if (doc) {
-            return users.updateById(doc._id, {
-              login: doc.login,
-              token: req.session.token
-            }, next);
-          }
-
-          // Insert user to database
-          users.insert({
-            login: user.login,
-            token: req.session.token
-          }, next);
-
-        });
-      });
-    }
-    return next();
-  }
-  next();
-});
+// Middleware to add GH user to request object
+app.use(require('./lib/github_user_middleware'));
 
 app.use(express.static('dist/'));
 
