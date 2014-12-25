9c2e6304f99b66b813a93d0be672a838c7e70dfd
Ian Macalinao <me@ian.pw>
2014-12-25 14:05:17 -0600

    Add god script

---
+++ scripts/god.js
@@ -0,0 +1,23 @@
+var P = require('bluebird');
+var user = require('../lib/user');
+var users = require('../lib/db').get('users');
+P.promisifyAll(users);
+
+var follow = process.argv[2];
+if (!follow) {
+  console.error('Usage: node scripts/follow <username>');
+  process.exit(1);
+}
+
+user.fromLogin(follow).then(function(user) {
+  return [user, users.updateAsync({
+    login: user.login
+  }, {
+    $set: {
+      god: true
+    }
+  })];
+}).spread(function(user) {
+  console.log('Made', user.login, 'a god');
+  process.exit(0);
+});
