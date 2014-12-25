8c0de6f1c760081c039f7e763dd8da55475fc139
Ian Macalinao <me@ian.pw>
2014-12-25 13:56:12 -0600

    Center follower

--- client/scripts/components/Follower.jsx
+++ client/scripts/components/Follower.jsx
@@ -4,7 +4,7 @@ module.exports = React.createClass({
   render: function() {
     var follower = this.props.follower;
     return (
-      <div className="text-center">
+      <div className="text-center follower">
         <img src={follower.avatar} className="img-rounded" width="50" height="50" />
         <p><a href={'https://github.com/' + follower.login}>{follower.login}</a></p>
       </div>
