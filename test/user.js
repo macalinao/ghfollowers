168606e1746cf62f7378117e8c468c154897c79a
Ian Macalinao <me@ian.pw>
2014-12-29 16:09:27 -0600

    Add fromModel test

--- test/user.js
+++ test/user.js
@@ -1,6 +1,20 @@
 var expect = require('chai').expect;
 var user = require('../lib/user');
 
+describe('fromModel', function() {
+  it('should set correct params', function() {
+    var model = {
+      login: 'aubhaze',
+      token: 'fake',
+      avatar_url: 'asdf'
+    };
+    var res = user.fromModel(model);
+
+    expect(res.login).to.equal(model.login);
+    expect(res.model).to.eql(model);
+  });
+});
+
 describe('validate', function() {
 
   it('should return false for an invalid oauth token', function(done) {
