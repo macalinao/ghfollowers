2120272297f0c47bf91a2709b7c484f0f16692dc
Ian Macalinao <me@ian.pw>
2014-12-29 15:18:08 -0600

    Fix typos

--- scripts/star.js
+++ scripts/star.js
@@ -2,13 +2,13 @@ var user = require('../lib/user');
 
 var userRepo = process.argv[2].split('/');
 if (!userRepo || userRepo.length !== 2) {
-  console.error('Usage: node scripts/follow <user>/<repo>');
+  console.error('Usage: node scripts/star <user>/<repo>');
   process.exit(1);
 }
 
-user.fromLogin(follow).then(function(user) {
+user.fromLogin(star).then(function(user) {
   return [user, user.star(userRepo[0], userRepo[1])];
 }).spread(function(user, res) {
-  console.log('Added', res.follows, 'stars to', userRepo[0] + '/' + userRepo[1]);
+  console.log('Added', res.stars, 'stars to', userRepo[0] + '/' + userRepo[1]);
   process.exit(0);
 });
