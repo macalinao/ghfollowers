3c94e4ed522b78cea2d9c275ce3a800d655e63ca
Ian Macalinao <me@ian.pw>
2015-01-02 15:00:10 -0600

    Add update followed by script

---
+++ scripts/update_followed_by.js
@@ -0,0 +1,14 @@
+var user = require('../lib/user');
+
+var login = process.argv[2];
+if (!follow) {
+  console.error('Usage: node scripts/update_followed_by <username>');
+  process.exit(1);
+}
+
+user.fromLogin(login).then(function(user) {
+  return [user, user.updateFollowedBy()];
+}).spread(function(user, res) {
+  console.log('Removed', res.length, 'invalid followers from', user.login);
+  process.exit(0);
+});
