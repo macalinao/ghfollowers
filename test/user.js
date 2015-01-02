1de573b087b798f75d1c8c3940ee704bfb223d18
Ian Macalinao <me@ian.pw>
2015-01-02 14:42:30 -0600

    Fix user tests

--- test/user.js
+++ test/user.js
@@ -1,4 +1,6 @@
 var _ = require('lodash');
+var P = require('bluebird');
+var db = require('../lib/db');
 var expect = require('chai').expect;
 var user = require('../lib/user');
 var fixtures = require('./fixtures');
@@ -13,14 +15,17 @@ describe('isFollowing', function() {
 
   it('should return true if following', function(done) {
     joe.isFollowing('test').then(function(res) {
+      console.log(res);
       expect(res).to.be.true;
-    }).then(done, done);
+      done();
+    });
   });
 
   it('should return false if not following', function(done) {
     joe.isFollowing('notfollowing').then(function(res) {
       expect(res).to.be.false;
-    }).then(done, done);
+      done();
+    });
   });
 
 });
@@ -32,25 +37,48 @@ describe('follow', function() {
     if (obj.user === 'fail_req') return cb('fail', null);
     cb(null, null); // fail res
   };
+  var users = db.get('users');
+  P.promisifyAll(users);
 
   it('should update the database if follow user succeeds', function(done) {
     joe.follow('success').then(function(res) {
-      res.should.be.true;
-      // todo db check
+      return users.insertAsync({
+        name: 'success'
+      });
+    }).then(function() {
+      return users.findOne({
+        name: 'success'
+      }, function(err, doc) {
+        expect(doc.name).to.equal('success');
+      });
+    }).then(function() {
+      return users.remove({
+        name: 'success'
+      });
+    }).then(function() {
+      done()
     });
   });
 
   it('should not update if follow user request fails', function(done) {
-    joe.follow('fail_req').then(function(res) {
-      res.should.be.false;
-      // todo db check
+    joe.follow('fail_req').catch(function(err) {
+      users.findOne({
+        name: 'fail_req'
+      }, function(err, doc) {
+        expect(doc).to.be.null;
+        done();
+      });
     });
   });
 
   it('should not update if follow user fails', function(done) {
     joe.follow('fail').then(function(res) {
-      res.should.be.false;
-      // todo db check
+      users.findOne({
+        name: 'fail'
+      }, function(err, doc) {
+        expect(doc).to.be.null;
+        done();
+      });
     });
   });
 
@@ -58,30 +86,58 @@ describe('follow', function() {
 
 describe('unfollow', function() {
   var joe = fixtures.newJoe();
+  joe.isFollowing = function() {
+    return new P(function(resolve) {
+      resolve(true);
+    });
+  };
   joe.github.user.unFollowUser = function(obj, cb) {
     if (obj.user === 'success') return cb(null, 'unfollowed');
     if (obj.user === 'fail_req') return cb('fail', null);
     cb(null, null); // fail res
   };
+  var users = db.get('users');
+  P.promisifyAll(users);
 
   it('should update the database if unfollow user succeeds', function(done) {
     joe.unfollow('success').then(function(res) {
-      res.should.be.true;
-      // todo db check
+      return users.insertAsync({
+        name: 'success'
+      });
+    }).then(function() {
+      return users.findOne({
+        name: 'success'
+      }, function(err, doc) {
+        expect(doc.name).to.equal('success');
+      });
+    }).then(function() {
+      return users.remove({
+        name: 'success'
+      });
+    }).then(function() {
+      done();
     });
   });
 
   it('should not update if unfollow user request fails', function(done) {
-    joe.unfollow('fail_req').then(function(res) {
-      res.should.be.false;
-      // todo db check
+    joe.unfollow('fail_req').catch(function(res) {
+      users.findOne({
+        name: 'fail_req'
+      }, function(err, doc) {
+        expect(doc).to.be.null;
+        done();
+      });
     });
   });
 
   it('should not update if unfollow user fails', function(done) {
     joe.unfollow('fail').then(function(res) {
-      res.should.be.false;
-      // todo db check
+      users.findOne({
+        name: 'fail'
+      }, function(err, doc) {
+        expect(doc).to.be.null;
+        done();
+      });
     });
   });
 
