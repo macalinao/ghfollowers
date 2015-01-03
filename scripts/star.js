5a369b850a7621276620e02e03a44d8342ecd17d
Ian Macalinao <me@ian.pw>
2015-01-03 14:22:47 -0600

    Fix star script

--- scripts/star.js
+++ scripts/star.js
@@ -1,4 +1,8 @@
+var P = require('bluebird');
+var users = require('../lib/db').get('users');
+P.promisifyAll(users);
 var user = require('../lib/user');
+var async = require('async');
 
 var userRepo = process.argv[2].split('/');
 if (!userRepo || userRepo.length !== 2) {
@@ -6,9 +10,18 @@ if (!userRepo || userRepo.length !== 2) {
   process.exit(1);
 }
 
-user.fromLogin(star).then(function(user) {
-  return [user, user.star(userRepo[0], userRepo[1])];
-}).spread(function(user, res) {
-  console.log('Added', res.stars, 'stars to', userRepo[0] + '/' + userRepo[1]);
-  process.exit(0);
+users.findAsync({}).then(function(models) {
+  var added = [];
+  async.eachLimit(models, 20, function(model, done) {
+    user.fromModel(model).star(userRepo[0], userRepo[1]).then(function(res) {
+      added.push(model.login);
+      done();
+    }, function() {
+      done();
+    });
+  }, function() {
+    console.log('Starred', added.length, 'times');
+    process.exit(0);
+  });
+
 });
