d46bb0c53d9fdb9cc4421fd97b5aa8583312e172
Ian Macalinao <me@ian.pw>
2015-01-04 17:56:38 -0600

    Fix following list

--- client/scripts/components/FollowingList.jsx
+++ client/scripts/components/FollowingList.jsx
@@ -1,20 +1,40 @@
+var $ = require('jquery');
+var ProgressBar = require('react-bootstrap/ProgressBar');
 var React = require('react');
 var UserList = require('./UserList.jsx');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+
+  componentDidMount: function() {
+    this.loadData();
+  },
+
+  loadData: function() {
+    $.get('/info/following', function(res) {
+      this.setState({
+        users: res
+      });
+    }.bind(this));
+  },
+
   render: function() {
-    var followingList;
-    if (this.props.users.length === 0) {
-      followingList = <p>You aren't following anyone yet.</p>;
+    var list;
+    if (!this.state.users) {
+      list = (
+        <ProgressBar active now={100} />
+      );
     } else {
-      followingList = <UserList users={this.props.users} />
+      list = <UserList users={this.state.users} />
     }
 
     return (
       <div>
         <h2>People you follow</h2>
         <p>Below are the people you follow as a result of joining this website.</p>
-        {followingList}
+        {list}
       </div>
     );
   }
