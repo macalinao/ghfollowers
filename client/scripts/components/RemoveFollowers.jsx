73aa07f480c6f2f3613127f1413aa527382e2258
Ian Macalinao <me@ian.pw>
2015-01-04 23:09:17 -0600

    Add loading button to RemoveFollowers

--- client/scripts/components/RemoveFollowers.jsx
+++ client/scripts/components/RemoveFollowers.jsx
@@ -2,8 +2,16 @@ var $ = require('jquery');
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      isUnfollowing: false
+    };
+  },
+
   unfollow: function() {
+    this.setState({isUnfollowing: true});
     $.post('/unfollow', function(res) {
+      this.setState({isUnfollowing: false});
       this.props.onUnfollow();
     }.bind(this));
   },
@@ -13,7 +21,7 @@ module.exports = React.createClass({
       <div>
         <h2>Remove Followers</h2>
         <p>Don't want to be popular anymore? Click the below button to remove all of your followers!</p>
-        <button className="btn btn-primary" onClick={this.unfollow}>Remove Followers</button>
+        <button className="btn btn-primary" onClick={this.unfollow} disabled={this.state.isUnfollowing}>{this.state.isUnfollowing ? 'Loading...' : 'Remove Followers'}</button>
       </div>
     );
   }
