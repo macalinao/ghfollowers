75c8955bf0ca559d4d79a96d454ae6ce2d24961a
Ian Macalinao <me@ian.pw>
2015-01-13 12:43:04 -0600

    Add copyright before i get into trouble

--- client/scripts/components/App.jsx
+++ client/scripts/components/App.jsx
@@ -32,6 +32,7 @@ var App = React.createClass({
           <div className="row padded">
             <div className="col-md-12 text-center">
               <p>Made by <a href="https://twitter.com/simplyianm">@simplyianm</a>. <a href="https://github.com/simplyianm/githubfollowers">View on GitHub</a></p>
+              <p>The Octocat logo is property of GitHub, Inc.</p>
             </div>
           </div>
         </div>
